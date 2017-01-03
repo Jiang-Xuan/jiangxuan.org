ee0f1c764aa7adb77874a9b4fd0c5aea6b6fc355
jiangxuan <jiangxuan@yuantutech.com>
2017-01-03 18:58:48 +0800

    慢慢的移除不属于自己的代码

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ var smart_ui_version = require('./package.json').smart_ui_version
 
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-var HtmlWebpackPlugin2 = require('./HtmlWebpackPlugin2')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
-// var dllConfig = require('./webpack.config.dll')
 var path = require('path')
 var customMDtoHTML = require('./customMDtoHTML')
 
@@ -15,26 +13,13 @@ var evnironment = process.env.NODE_ENV || 'dev'
 
 var dev_environment = evnironment.indexOf('dev') !== -1
 
-var daily_environment = evnironment.indexOf('daily') !== -1
 var dist_environment = evnironment.indexOf('dist') !== -1
 
 var rootPath = './src-react'
 var distPath = './build'
 
-var daily_cdn_domain = 'daily.yuantutech.com'
-var dist_cdn_domain = 's.yuantutech.com'
-
-var cnd_domain = daily_environment || dev_environment ? daily_environment : dist_cdn_domain
-
-//cdn
-var daily_publicPath = "//daily.yuantutech.com/yuantu/h5-cli/"+version+"/";
-var dist_publicPath = "//s.yuantutech.com/yuantu/h5-cli/"+version+"/";
-
 // 本地开发环境通常不需要配置
 var dev_publicPath = null
-
-
-var publicPath = daily_environment ? daily_publicPath : (dist_environment ? dist_publicPath : dev_publicPath)
 
 var extractLess = new ExtractTextPlugin('all.css')
 
@@ -54,7 +39,7 @@ var config = {
   },
   output: {
     path: path.resolve(__dirname, distPath),
-    publicPath: publicPath,
+    publicPath: "./",
     filename: '[name].bundle.js'
   },
   module: {
@@ -88,53 +73,12 @@ var config = {
   ]
 }
 
-var addAssetTags = [
-  {
-    tagName: 'script',
-    closeTag: true,
-    attributes: {
-      type: 'text/javascript',
-      src: (function() {
-        if(dev_environment) {
-          return `//s.yuantutech.com/yuantu/h5-cli/1.4.29/vendor.js`
-        }
-
-        if(daily_environment) {
-          return  `//s.yuantutech.com/yuantu/h5-cli/1.4.29/vendor.js`
-        }
-
-        if(dist_environment){
-          return  `//s.yuantutech.com/yuantu/h5-cli/1.4.29/vendor.js`
-        }
-      })()
-    }
-  }
-]
-
-var addAfterTags = [
-  {
-    tagName: 'script',
-    closeTag: true,
-    attributes: {
-      type: 'text/javascript',
-      async:"async",
-      src:"https://s.yuantutech.com/yuantu/spm/1.0.7/??spm.js,track.js"
-    }
-  }
-]
-
-HtmlWebpackPlugin2.prototype.injectAssetsIntoAssetTags = function(assetTags) {
-  // assetTags.body = addAssetTags.slice(0).concat(assetTags.body)
-  // assetTags.body = assetTags.body.concat(addAfterTags)
-  return assetTags
-};
-
 //打包的时候把静态资源打包进去 
 Object.keys(entry).map(function(key){
   if( fs.existsSync( path.resolve(rootPath, `${key}.html`) )){
     console.log(`ADD entry ${key}.html`);
     config.plugins.push(
-      new HtmlWebpackPlugin2({
+      new HtmlWebpackPlugin({
           filename:path.resolve(distPath, `${key}.html`),
           template:path.resolve(rootPath, `${key}.html`),
           inject:true,
@@ -145,11 +89,6 @@ Object.keys(entry).map(function(key){
           minify:{
             removeComments:false,
             collapseWhitespace:false
-          },
-          //HtmlWebpackPlugin2 实现了变量插入
-          variable:{
-            cnd_domain:cnd_domain,
-            smart_ui_version:smart_ui_version
           }
       })
     )
@@ -176,7 +115,7 @@ Object.keys(mdEntry).map(function(item) {
     })
   )
 })
-
+// -----------------markdown end-----------
 //线上打包需要压缩代码
 if(dist_environment){
   config.plugins.unshift(
